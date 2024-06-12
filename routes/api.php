f58ca6716577d5627f096f7c568942860be8c7ad
unknown <diandraregita14@gmail.com>
2024-06-12 14:18:05 +0700

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TasksController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// Public routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+// Protected routes
+Route::group(['middleware'=> ['auth:sanctum']], function () {
+    Route::resource('/tasks', TasksController::class);
+    Route::post('/logout', [AuthController::class,'logout']);
+});
